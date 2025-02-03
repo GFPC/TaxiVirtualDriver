9f0f8dfd5453b05fe17721d611b2bb25768b5760
GFP <fealx15@gmail.com>
2025-02-04 00:45:02 +0700

    1

--- api_lib.py
+++ api_lib.py
@@ -61,6 +61,20 @@ def GetAdminHashAndToken():
         U_HASH = r[1]
         return [TOKEN, U_HASH]
 
+
+def NowDrivesList():
+    token, u_hash = GetAdminHashAndToken()
+
+    data = {
+        "token": token,
+        "u_hash": u_hash,
+
+        "u_a_role": 2,
+    }
+
+    data = make_request(url_prefix + "drive/now", data=data)
+    return data
+
 # Возвращает token и u_hash пользователя, а также данные о пользователе
 def GetUserInfo(email:str):
     token_and_hash = GetAdminHashAndToken()
@@ -133,6 +147,4 @@ def CancelDrive(drive_id,reason:str):
     data = make_request(url_prefix + "drive/get/"+str(drive_id), data=data)
     return data
 
-def NowDrivesList():
-    return {"data":{"booking":{}}}
 print("TEST POINT 3")
