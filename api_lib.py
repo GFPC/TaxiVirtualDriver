06f0b0784b2718bb7f96f29c32b2c95285f69206
GFP <fealx15@gmail.com>
2025-02-04 00:40:24 +0700

    1

--- api_lib.py
+++ api_lib.py
@@ -31,3 +31,105 @@ def make_request(url, data={}, method="POST"):
 # GetAdminHashAndToken получает хэш админа и токен и хранит их в указанном файле(создается если его нет)
 # это сделано для сокращения времени запросов, требующих авторизации админа
 # token и u_hash не меняются во время работы
+def GetAdminHashAndToken():
+    try:
+        f = open(ADMIN_CREDENTIALS_FILE, "x")
+        f.close()
+    except:
+        pass
+    f = open(ADMIN_CREDENTIALS_FILE, "r+")
+    r = f.read().split("\n")
+    if len(r) == 1 or len(r) == 0:
+        data = {
+            "login": bot_admin_login,
+            "password": bot_admin_password,
+            "type": bot_admin_type
+        }
+        data = make_request(url_prefix + "auth", data)
+        AUTH_HASH = data["auth_hash"]
+        data = {
+            "auth_hash": AUTH_HASH
+        }
+        data = make_request(url_prefix + "token", data)
+        TOKEN = data["data"]["token"]
+        U_HASH = data["data"]["u_hash"]
+        f.write(TOKEN + "\n" + U_HASH)
+        f.close()
+        return [TOKEN, U_HASH]
+    elif len(r) == 2:
+        TOKEN = r[0]
+        U_HASH = r[1]
+        return [TOKEN, U_HASH]
+
+# Возвращает token и u_hash пользователя, а также данные о пользователе
+def GetUserInfo(email:str):
+    token_and_hash = GetAdminHashAndToken()
+    data = {
+        "token": token_and_hash[0],
+        "u_hash": token_and_hash[1],
+        "u_a_email": email,
+    }
+    data = make_request(url_prefix + "token", data=data)
+    if data["status"] == "error":
+        return data
+    token = data["data"]["token"]
+    u_hash = data["data"]["u_hash"]
+    data = make_request(url_prefix + "user", data={
+        "token": data["data"]["token"],
+        "u_hash": data["data"]["u_hash"]
+    })
+    data["data"]["token"] = token
+    data["data"]["u_hash"] = u_hash
+    return data
+def RegisterClient(email:str, name):
+    token_and_hash = GetAdminHashAndToken()
+    data = {
+        "token": token_and_hash[0],
+        "u_hash": token_and_hash[1],
+        "u_name": name,
+        "u_email": email,
+        "u_role": "2",
+        #"ref_code": "test",
+        "st": ""
+    }
+    data = make_request(url_prefix+"register",data=data,method="POST")
+    return data
+def CreateDrive(u_id,start_latitude,start_longitude,end_latitude,end_longitude,start_datetime,waiting,passenger_count=1,services=[]):
+    token,u_hash = GetAdminHashAndToken()
+    data = {
+        "token": token,
+        "u_hash": u_hash,
+
+        "u_a_id":str(u_id), # Авторизован(по токену и хэшу) админ, а drive создастя от лица юзера - имиация пользователя админом
+        "u_a_role":1,
+        "u_check_state": 2,
+        "data":json.dumps({
+            "b_start_latitude": start_latitude,
+            "b_start_longitude": start_longitude,
+            "b_destination_latitude": end_latitude,
+            "b_destination_longitude": end_longitude,
+            "b_start_datetime": start_datetime,
+            "b_max_waiting": waiting,
+            "b_passengers_count": passenger_count,
+            "b_payment_way": "1",
+            "b_services": services
+        })
+
+    }
+    data = make_request(url_prefix + "drive/", data=data)
+    return data
+
+def CancelDrive(drive_id,reason:str):
+    token,u_hash = GetAdminHashAndToken()
+    data = {
+        "token": token,
+        "u_hash": u_hash,
+
+        "u_a_role":4, # user_role=4 for deleting drive under admin
+
+        "action":"set_cancel_state",
+        "reason":reason,
+    }
+    data = make_request(url_prefix + "drive/get/"+str(drive_id), data=data)
+    return data
+
